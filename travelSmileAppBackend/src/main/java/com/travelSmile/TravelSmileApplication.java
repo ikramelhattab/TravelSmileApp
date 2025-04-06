c9cd33f811877157d83698e82fbeb6d065e4aca5
ikramelhattab <ikramelhattab@gmail.com>
2025-04-06 02:27:46 +0100

    update main files

---
+++ travelSmileAppBackend/src/main/java/com/travelSmile/TravelSmileApplication.java
@@ -0,0 +1,11 @@
+package com.travelsmile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSmileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TravelSmileApplication.class, args);
+    }
+}

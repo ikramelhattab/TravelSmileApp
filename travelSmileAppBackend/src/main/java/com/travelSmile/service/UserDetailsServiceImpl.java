3048f2786fda8eb1f00982dc84262184c36dc02d
ikramelhattab <ikramelhattab@gmail.com>
2025-04-06 02:21:49 +0100

    service files

---
+++ travelSmileAppBackend/src/main/java/com/travelSmile/service/UserDetailsServiceImpl.java
@@ -0,0 +1,18 @@
+package com.travelsmile.service;
+
+import com.travelsmile.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
